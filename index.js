569c76b9cfa154b91562cd96e1e8e214791a10f9
travis_gandy <travis_gandy@yahoo.com>
2021-04-16 08:37:01 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, func){
+        var i, l,
+            temp = [];
+        for (i = 0, l = array.length; i < l; i++) {
+            temp.push(func(array[i]));
+        }
+        return temp;
+    
+}
+
+
+function reduce(array, func, startingVal = 0){
+    if (startingVal > 0){
+       return array.reduce(func, startingVal)
+    } else {
+        return array.reduce(func)
+    }
+    }
